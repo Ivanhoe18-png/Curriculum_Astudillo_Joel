d0dab309634c45a2a3d39585577cd4e66d36dd78
Ivanhoe18-png <139147967+Ivanhoe18-png@users.noreply.github.com>
2023-07-10 10:32:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Curriculum_Astudillo_Joel
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-q28yyd)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('aplicacion_CV1');
+appDiv.innerHTML = `<h1>Hoja de Vida Joel Astudillo</h1>`;
